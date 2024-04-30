ac3f770c8b5d1b86f0e28433ce7219679a70cde5
john <tatamarcas273@gmail.com>
2024-04-30 18:59:52 +0200

    first commit

---
+++ portfolio/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import  AOS from 'aos';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent  implements OnInit {
+  title = 'portfolio';
+  ngOnInit(){
+    AOS.init();
+   }
+}
